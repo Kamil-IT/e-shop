4b4b46ab98262abf92ae662cf470b739f853bb34
kamil.wolny <external.kamil.wolny@kuehne-nagel.com>
2021-09-24 17:56:04 +0200

    Add script to create db

---
+++ db/initdb/inittestdb.sql
@@ -0,0 +1,61 @@
+CREATE TABLE user
+(
+    id         INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name  VARCHAR(100),
+    e_mail     VARCHAR(100),
+    adress     VARCHAR(200)
+);
+CREATE INDEX idx_shopping_cart_2
+    ON shopping_cart_item (product_id);
+
+CREATE TABLE products
+(
+    id           INT PRIMARY KEY,
+    name         VARCHAR(100),
+    price        FLOAT,
+    quantity     INT,
+    availability VARCHAR(3),
+    weight       FLOAT
+);
+
+
+CREATE TABLE shopping_cart
+(
+    id             INT PRIMARY KEY,
+    quantity       INT,
+    total_price    INT,
+    shipping_price FLOAT
+);
+CREATE INDEX product_idx
+    ON products (id, name, availability);
+
+CREATE TABLE order_x
+(
+    id           INT PRIMARY KEY,
+    user_id      INT,
+    total_price  INT,
+    total_weight FLOAT
+);
+ALTER TABLE order_x
+    ADD CONSTRAINT order_x_ibfk_
+        FOREIGN KEY (user_id)
+            REFERENCES user (id);
+CREATE INDEX idx_shopping_cart
+    ON shopping_cart_item (product_id);
+
+CREATE TABLE shopping_cart_item
+(
+    shopping_cart_id INT,
+    product_id       INT,
+    quantity         INT,
+    PRIMARY KEY (shopping_cart_id, product_id)
+);
+ALTER TABLE shopping_cart_item
+    ADD CONSTRAINT shopping_cart_item_ibfk_
+        FOREIGN KEY (shopping_cart_id)
+            REFERENCES shopping_cart (id);
+ALTER TABLE shopping_cart_item
+    ADD CONSTRAINT shopping_cart_item_ibfk_2
+        FOREIGN KEY (product_id)
+            REFERENCES products (id);
